da290a577d86e68aefe96451a9aebf09731896e7
sairamkumarm21 <sairamkumarm21@gmail.com>
2025-09-21 22:12:58 +0530

    Centralised logger and used RFC3339 as timeEncoding

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -3,14 +3,18 @@ package logger
 import (
 	"strings"
 
+	"github.com/sairamkumarm/gositemonitor/pkg/scrapper"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
-func New(level string) *zap.Logger {
+var Log *zap.Logger
+
+func New(level string) {
 
 	cfg := zap.NewProductionConfig()
 	cfg.Encoding = "json"
-
+	cfg.EncoderConfig.EncodeTime = zapcore.RFC3339TimeEncoder //uses time format 2025-09-21T18:32:00Z
 	switch strings.ToLower(level) {
 	case "info":
 		cfg.Level = zap.NewAtomicLevelAt(zap.InfoLevel)
@@ -21,7 +25,32 @@ func New(level string) *zap.Logger {
 	default:
 		cfg.Level = zap.NewAtomicLevelAt(zap.InfoLevel)
 	}
-
+	
 	logger, _ := cfg.Build()
-	return logger
+	Log = logger
+}
+
+func ResultLogger(res scrapper.ScrapeResult){
+	switch {
+			case res.Status == -1:
+				Log.Error("Ping Failed",
+					zap.String("URL", res.URL),
+					zap.Time("TimeStampUTC", res.TimestampUTC),
+					zap.String("Error", res.Error),
+					zap.Int("WorkerID", res.WorkerID))
+			case res.Status >= 400:
+				Log.Warn("Non-2XX Status",
+					zap.String("URL", res.URL),
+					zap.Time("TimeStampUTC", res.TimestampUTC),
+					zap.Int("Status", res.Status),
+					zap.Int("Latency", int(res.ResponseMS)),
+					zap.Int("WorkerID", res.WorkerID))
+			default:
+				Log.Info("Ping Success",
+					zap.String("URL", res.URL),
+					zap.Time("TimeStampUTC", res.TimestampUTC),
+					zap.Int("Status", res.Status),
+					zap.Int("Latency", int(res.ResponseMS)),
+					zap.Int("WorkerID", res.WorkerID))
+			}
 }
